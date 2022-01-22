e7509854929245f0184ed3523a9c6662d3bbe7ff
thennek0 <kennethbecaro0@gmail.com>
2022-01-22 12:14:03 +0800

    grid create and grid remove

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 const container = document.querySelector('.container');
 
-for (let x = 0; x < 16; x++) {
-    const rows = document.createElement('div');
-    rows.classList.add('rows');
+let size = 16;
+createGrid(size);
 
-    for (let z = 0; z < 16; z++) {
-        const box = document.createElement('div');
-        box.classList.add('boxes');
-        rows.appendChild(box);
+function createGrid(size) {
+    for (let x = 0; x < size; x++) {
+        const rows = document.createElement('div');
+        rows.classList.add('rows');
+    
+        for (let z = 0; z < size; z++) {
+            const box = document.createElement('div');
+            box.classList.add('boxes');
+            rows.appendChild(box);
+        }
+        container.appendChild(rows);
     }
-    container.appendChild(rows);
 }
-
 const gridBoxes = document.querySelectorAll('.boxes');
-gridBoxes.forEach(box => box.addEventListener('mouseenter',e => e.target.classList.add('hover')));
+gridBoxes.forEach(box => box.addEventListener('mouseover',e => e.target.classList.add('hover')));
+
+const rows = document.querySelectorAll('.rows');
+
+function removeGrid() {
+    gridBoxes.forEach(element => {
+        element.remove();
+    });
+    rows.forEach(element => {
+        element.remove();
+    });
+}
 
 const clearButton = document.querySelector('.clear');
 
+// forEach(box => {
+//     box.addEventLister("hover", (e) => e.target.classList.add('.hover'))
+// });
 clearButton.addEventListener('click',removeClass);
 
 function removeClass() {
     for (let index = 0; index < gridBoxes.length; index++) {
         gridBoxes[index].classList.remove('hover');
     }
-    
+    removeGrid();
+    size = prompt("eneter number");
+    createGrid(parseInt(size));
 }
