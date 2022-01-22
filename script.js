6ee968f5ff0d4f943dd4bcafc2b13b3f269450d8
thennek0 <kennethbecaro0@gmail.com>
2022-01-22 15:27:45 +0800

    finished functionality

--- script.js
+++ script.js
@@ -3,28 +3,33 @@ const container = document.querySelector('.container');
 let size = 16;
 createGrid(size);
 
+
+
 function createGrid(size) {
     for (let x = 0; x < size; x++) {
         const rows = document.createElement('div');
         rows.classList.add('rows');
-    
+        container.appendChild(rows);
         for (let z = 0; z < size; z++) {
             const box = document.createElement('div');
             box.classList.add('boxes');
             rows.appendChild(box);
         }
-        container.appendChild(rows);
     }
-}
-const gridBoxes = document.querySelectorAll('.boxes');
-gridBoxes.forEach(box => box.addEventListener('mouseover',e => e.target.classList.add('hover')));
+    const gridBoxes = document.querySelectorAll('.boxes');
+    gridBoxes.forEach(box => box.addEventListener('mouseover',e => e.target.classList.add('hover')));
 
-const rows = document.querySelectorAll('.rows');
+}
+
+
+
 
 function removeGrid() {
+    const gridBoxes = document.querySelectorAll('.boxes');
     gridBoxes.forEach(element => {
         element.remove();
     });
+    const rows = document.querySelectorAll('.rows');
     rows.forEach(element => {
         element.remove();
     });
@@ -32,16 +37,16 @@ function removeGrid() {
 
 const clearButton = document.querySelector('.clear');
 
-// forEach(box => {
-//     box.addEventLister("hover", (e) => e.target.classList.add('.hover'))
-// });
 clearButton.addEventListener('click',removeClass);
 
 function removeClass() {
+    const gridBoxes = document.querySelectorAll('.boxes');
     for (let index = 0; index < gridBoxes.length; index++) {
         gridBoxes[index].classList.remove('hover');
     }
     removeGrid();
-    size = prompt("eneter number");
-    createGrid(parseInt(size));
+    size = prompt("enter number");
+    createGrid(size);
+
 }
+
